15e3240468bfdc1169afc6ae71191d0599384102
Sukera <Seelengrab@users.noreply.github.com>
2024-05-14 12:50:14 +0200

    Improve desync a bit

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 [`Bad Apple!!`](https://www.youtube.com/watch?v=i41KoE0iMYU) rendered using [StyledStrings.jl](https://github.com/JuliaLang/StyledStrings.jl)
 
 This isn't doing anything fancy, just displaying the raw `.webm` frames through `styled""`. It would probably be smarter to do more
-preprocessing so that continous runs can be colored at once.
+preprocessing so that continuous runs can be colored at once.
+
+When run in a terminal, this also attempts to play audio through `ffplay`. I have not taken too much care that everything is synced up
+(the duration is off by about 0.2s on my machine), but the playback should (roughly) happen with 30 FPS :)
 
 You can view a partial recording here:
 
--- apple.jl
+++ apple.jl
@@ -13,11 +13,17 @@ function main(; stats=false, volume=80)
     video_file = joinpath(@__DIR__, "Bad Apple!! feat.nomico 40h.webm")
     badapple = VideoIO.openvideo(video_file)
     buf = Base.AnnotatedIOBuffer()
+    cond = Condition()
+    audio_start = Ref{Float64}()
+    audio_end = Ref{Float64}()
 
     # ffplay takes 0-100
     vol = round(Int, clamp(volume, 0, 100))
     audio = @async ffplay() do exe
+        wait(cond)
+        audio_start[] = time()
         success(`$exe -autoexit -nodisp -volume $vol $video_file`)
+        audio_end[] = time()
     end
 
     # clear the screen
@@ -25,6 +31,7 @@ function main(; stats=false, volume=80)
 
     # start running!
     old_time = time()
+    start_time = time()
     for frame in badapple
         seekstart(buf)
         write(buf, "\e[H")
@@ -39,17 +46,33 @@ function main(; stats=false, volume=80)
         seekstart(buf)
         write(stdout, buf)
         flush(stdout)
-        t = time() - old_time
-        sleep_time = max(0, 1/30 - t)
-        frametime = round(t; digits=5)
-        stats && print(styled"""
-        {grey:Frametime:     $frametime
-        Sleep:         $(round(sleep_time; digits=5))}
-        """)
-        sleep(sleep_time)
+        notify(cond)
+        cur_time = time()
+        render_time = cur_time - old_time
+        if stats
+            frametime = round(render_time; digits=5)
+            print(styled"""
+            {grey:Frametime:     $frametime}""")
+        end
+        while time() < (cur_time+(1/29.97-render_time))
+            # busy wait, task yielding is expensive
+        end
         old_time = time()
     end
-    wait(audio)
+
+    if stats
+        total_time = time() - start_time
+        mins, secs = divrem(total_time, 60)
+        imins = round(Int, mins)
+        secs = round(secs, sigdigits=3)
+        println("\nVideo Duration: $(imins)m $(secs)s")
+        wait(audio)
+        audio_time = audio_end[] - audio_start[]
+        mins, secs = divrem(audio_time, 60)
+        imins = round(Int, mins)
+        secs = round(secs, sigdigits=3)
+        println("Audio Duration: $(imins)m $(secs)s")
+    end
 end
 
-!isinteractive() && main()
+!isinteractive() && main(;stats=true)
